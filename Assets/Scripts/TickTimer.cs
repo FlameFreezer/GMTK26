cda870dc76c764e8f99a65e9883c33361d9d7690
FFreeze <quincymon@icloud.com>
2026-07-22 20:55:39 -0700

    isPaused flag added to Tick Timer

--- Assets/Scripts/TickTimer.cs
+++ Assets/Scripts/TickTimer.cs
@@ -5,6 +5,7 @@ public class TickTimer : MonoBehaviour
 {
     public double _ticksPerSecond;
     private double _timeSinceLastTick = 0.0;
+    private bool _isPaused = false;
     public Action _tickUpdate;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -16,12 +17,15 @@ public class TickTimer : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        _timeSinceLastTick += Time.deltaTime;
-        double secondsPerTick = 1.0 / _ticksPerSecond;
-        if (_timeSinceLastTick >= secondsPerTick)
+        if(!_isPaused)
         {
-            _tickUpdate?.Invoke();
-            _timeSinceLastTick -= secondsPerTick;
+            _timeSinceLastTick += Time.deltaTime;
+            double secondsPerTick = 1.0 / _ticksPerSecond;
+            if (_timeSinceLastTick >= secondsPerTick)
+            {
+                _tickUpdate?.Invoke();
+                _timeSinceLastTick -= secondsPerTick;
+            }
         }
     }
     private void OnTick()
