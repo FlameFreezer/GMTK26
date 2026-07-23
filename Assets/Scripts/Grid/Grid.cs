4de61c06a00be7bbe13259ad3ed4b54773db91ed
Gloomsfield <amory@acosta.boo>
2026-07-23 13:01:25 -0700

    write SpawnPlantAtGridPosition method

--- Assets/Scripts/Grid/Grid.cs
+++ Assets/Scripts/Grid/Grid.cs
@@ -1,10 +1,11 @@
+using System;
 using System.Collections.Generic;
 
 public class Grid {
     private List<Plant> _plants;
 
-	public int width;
-	public int height;
+	public UInt32 width;
+	public UInt32 height;
 
 	public void InvokeTick(PlantTypes.Type type) {
 		foreach(Plant plant in _plants) {
@@ -14,12 +15,31 @@ public class Grid {
 		}
 	}
 
-	public bool GetPlantAtGridPosition(int x, int y, out Plant result) {
+	public bool GetPlantAtGridPosition(UInt32 x, UInt32 y, out Plant result) {
 		result = null;
 
 		if(_plants.Count < y * width + x) { return false; }
 
-		result = _plants[y * width + x];
+		result = _plants[(int)(y * width + x)];
 		return true;
+	}
+
+	// assumes there's not already a plant at this position.
+	// TODO -- should we handle checking validity at the
+	// input level?
+	public void SpawnPlantAtGridPosition(UInt32 x, UInt32 y, PlantTypes.Type type) {
+		UInt32 plantId = y * width + x;
+
+		Plant newPlant = null;
+
+		switch(type) {
+			case PlantTypes.Type.EYE_WEED:
+				newPlant = new EyeWeed();
+				break;
+			default: return; // TODO - send an error
+		}
+
+		newPlant.AssignId(plantId);
+		_plants[(int)plantId] = newPlant;
 	}
 }
--- Assets/Scripts/Plants/Plants.cs
+++ Assets/Scripts/Plants/Plants.cs
@@ -1,11 +1,28 @@
 using System;
 
 public abstract class Plant {
-	protected int _ticksUntilHarvest = 0;
-
 	public PlantTypes.Type type;
 
-    public event Action OnHarvestRequested;
+	protected int _ticksUntilHarvest = 0;
+	protected UInt32 _id = UInt32.MaxValue;
+
+    public event Action<UInt32> OnHarvestRequested;
+
+	public void AssignId(UInt32 id) {
+		_id = id;
+	}
 
 	public abstract void Tick();
+
+	protected void InvokeOnHarvestRequested() {
+		OnHarvestRequested?.Invoke(_id);
+	}
+}
+
+public class EyeWeed : Plant {
+	public override void Tick() {
+		if(_ticksUntilHarvest < 1) {
+			InvokeOnHarvestRequested();
+		}
+	}
 }
