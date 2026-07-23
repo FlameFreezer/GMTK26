ac2c83912e9c995778d5d50f878d75f1c58b3568
Gloomsfield <amory@acosta.boo>
2026-07-23 12:14:53 -0700

    add type member to Plant class

--- Assets/Scripts/Plants/Plants.cs
+++ Assets/Scripts/Plants/Plants.cs
@@ -3,6 +3,8 @@ using System;
 public abstract class Plant {
 	protected int _ticksUntilHarvest = 0;
 
+	public PlantTypes.Type type;
+
     public event Action OnHarvestRequested;
 
 	public abstract void Tick();
